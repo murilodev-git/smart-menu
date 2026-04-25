59732cd2983d1b6bfee9899a30023b7de4bf46b9
Murilo Bacetti <murilo.a31@aluno.ifsc.edu.br>
2026-04-25 12:40:06 -0300

    Ajustando porta para deploy no Render

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs-extra');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const DB = './db.json';
 
 app.use(express.json());
